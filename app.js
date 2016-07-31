467b74fd81f74ea3e20b373ee109267385b1d8a5
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2016-07-31 11:42:21 +0200

    use the specific port to connect to the base

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ seneca = require('seneca')({
       { pin: ' role: cloudinary' }
       ] ,
       // required to be detect the base
-    bases: ['127.0.0.1:3000','mohamed.irehearse.com']
+    bases: ['127.0.0.1:39001','mohamed.irehearse.com:39001']
     //host: 'q3.my-domain.com'
   });
 
